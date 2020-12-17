bde09dcb415f64c0af83c21f634897aadb3eb451
towangfoo <towangfoo@posteo.net>
2020-12-17 17:19:51 +0100

    Statische Seite: Kontakt

--- front-page/newsletter.php
+++ front-page/newsletter.php
@@ -9,23 +9,6 @@
  * @var $post WP_Post <-- the front page
  */
 
+get_template_part( 'shortcodes/newsletter-signup' );
+
 ?>
-
-<div class="front-newsletter mt-6 mb-6" id="newsletter-signup">
-    <div class="row">
-        <div class="col-md-8 newsletter-container">
-            <h2><?php echo __("Newsletter", 'bufu-theme') ?></h2>
-            <p><?php echo __("Subscribe to our newsletter and stay up to date about upcoming concerts, new products and more information around the BuschFunk.", 'bufu-theme') ?></p>
-
-            <div class="newsletter-signup-form">
-				<?php bufu_rapidmail()->echoSignupForm([
-					'submit_button_label' => __('Sign up', 'bufu-theme'),
-                    'container_id'        => 'newsletter-signup',
-                ]) ?>
-            </div>
-        </div>
-        <div class="col-md-4 pt-4 pb-4 text-center">
-            <img width="300" src="<?php echo get_theme_file_uri('inc/assets/img/buschfunk30_sticker.jpg') ?>" alt="<?php echo __("30 years of BuschFunk", 'bufu-theme') ?>">
-        </div>
-    </div>
-</div>
--- functions.php
+++ functions.php
@@ -70,6 +70,8 @@ function wp_bootstrap_starter_setup() {
     }
     add_action( 'admin_init', 'wp_boostrap_starter_add_editor_styles' );
 
+    // add shortcode handling
+	add_shortcode( 'bufu_newsletter_signup', 'wp_bootstrap_shortcode_newsletter_signup' );
 }
 endif;
 add_action( 'after_setup_theme', 'wp_bootstrap_starter_setup' );
@@ -215,6 +217,12 @@ function wp_bootstrap_starter_preload( $hints, $relation_type ){
 
 add_filter( 'wp_resource_hints', 'wp_bootstrap_starter_preload', 10, 2 );
 
+
+function wp_bootstrap_shortcode_newsletter_signup( $atts, $content, $shortcode_tag ) {
+	ob_start();
+	get_template_part( 'shortcodes/newsletter-signup' );
+	return ob_get_clean();
+}
 
 
 function wp_bootstrap_starter_password_form() {
