0945973a4762d3a7620141d04a09832bf326997b
towangfoo <towangfoo@posteo.net>
2021-03-05 21:20:32 +0100

    Widget für anpassbares Mitteilungsarchiv

--- archive.php
+++ archive.php
@@ -1,19 +1,17 @@
 <?php
 /**
- * The template for displaying archive pages
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * Display archive pages for default post types
  *
  * @package BuschFunk
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-sm-12 col-lg-8">
-		<main id="main" class="site-main" role="main">
+    <section id="primary" class="content-area col-sm-12 col-md-12 col-lg-8">
+        <main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
+			<?php
+			if ( have_posts() ) : ?>
 
 			<header class="page-header">
 				<?php
@@ -22,33 +20,34 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
-            <div class="list-posts">
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                <div class="list-posts">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+						if ( is_single() ) {
+							get_template_part( 'template-parts/content-single', get_post_format() );
+						}
+						else {
+							get_template_part( 'template-parts/content-list', get_post_format() );
+						}
 
-			endwhile;
-			?>
-            </div>
+					endwhile;
 
-            <?php
-			get_template_part( 'pagination' );
+					?>
+                </div>
 
-		else :
+				<?php
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part( 'pagination' );
 
-		endif; ?>
+			else :
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+				get_template_part( 'template-parts/content-none' );
+
+			endif; ?>
+        </main><!-- #main -->
+    </section><!-- #primary -->
 
 <?php
 get_sidebar();
