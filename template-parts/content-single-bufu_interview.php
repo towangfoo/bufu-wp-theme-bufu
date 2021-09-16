286ee2c75b8d3ed9a3a93cdf656afc1e904082b6
towangfoo <towangfoo@posteo.net>
2021-09-16 16:05:39 +0200

    Interviews und Rezensionen im Theme anzeigen

--- template-parts/content-single-bufu_interview.php
+++ template-parts/content-single-bufu_interview.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying a single album.
+ * Template part for displaying a single interview.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -9,15 +9,13 @@
  * @var $post WP_Post
  */
 
-$thumbnailHtml = get_the_post_thumbnail( null, 'medium', ['class' => 'w-100'] );
-$hasThumbnail = $thumbnailHtml !== '';
-
 /** @var $artist WP_Post */
 $artist = $post->bufu_artist;
 
-$shopUrl      = get_post_meta( $post->ID, '_bufu_artist_shopUrl', true);
-$label        = get_post_meta( $post->ID, '_bufu_artist_albumLabel', true);
-$releaseDate  = get_post_meta( $post->ID, '_bufu_artist_albumRelease', true);
+$thumbnailHtml = get_the_post_thumbnail( $artist, 'medium', ['class' => 'w-100'] );
+$hasThumbnail = $thumbnailHtml !== '';
+
+$source = get_post_meta( $post->ID, '_bufu_artist_interview_source', true);
 
 ?>
 
@@ -28,14 +26,9 @@ $releaseDate  = get_post_meta( $post->ID, '_bufu_artist_albumRelease', true);
             if ($hasThumbnail) :
                 echo $thumbnailHtml;
             else :
-                echo bufu_theme_no_thumbnail(__("No album cover", 'bufu-theme'));
+                echo bufu_theme_no_thumbnail(__("No image", 'bufu-theme'));
             endif;
         ?>
-            <?php if (!empty($shopUrl)) : ?>
-                <div class="mt-3 mb-3">
-                    <a href="<?php echo esc_url( $shopUrl ) ?>" target="_blank" class="btn btn-primary btn-pill" data-external><?php echo __("Visit in shop", 'bufu-theme') ?></a>
-                </div>
-            <?php endif; ?>
 
             <div class="mt-3 mb-5">
                 <a href="<?php echo get_permalink( $artist ) ?>" class="btn btn-default btn-pill btn-sm no-uppercase no-nowrap"><?php echo sprintf(__("Artist portrait of %s", 'bufu-theme'), esc_html($artist->post_title)) ?></a>
@@ -44,14 +37,14 @@ $releaseDate  = get_post_meta( $post->ID, '_bufu_artist_albumRelease', true);
         </div>
         <div class="col-md-8">
             <header class="entry-header">
-                <h1 class="entry-title"><?php echo esc_html($artist->post_title) ?>: <?php echo get_the_title() ?></h1>
-                <p class="album-meta"><?php if (!empty($label)) echo trim($label) . "," ?> <?php echo $releaseDate ?></p>
+                <h1 class="entry-title"><?php echo get_the_title() ?></h1>
+                <p class="album-meta"><?php if (!empty($source)) echo esc_html($source) ?></p>
             </header><!-- .entry-header -->
             <div class="entry-content">
                 <?php the_content(); ?>
 
                 <div class="mt-5 mb-5">
-				    <a href="<?php echo get_permalink( $artist ) ?>" class="btn btn-default btn-pill">« <?php echo sprintf(__("Back to artist portrait of %s", 'bufu-theme'), esc_html($artist->post_title)) ?></a>
+                    <a href="<?php echo get_permalink( $artist ) ?>" class="btn btn-default btn-pill">« <?php echo sprintf(__("Back to artist portrait of %s", 'bufu-theme'), esc_html($artist->post_title)) ?></a>
                 </div>
             </div><!-- .entry-content -->
 
